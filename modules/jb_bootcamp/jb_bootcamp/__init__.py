fb4bff130e35c6d71952b92d05b1322eb8ca970f
Mengcheng Wu <81577363+wu709621493@users.noreply.github.com>
2026-02-28 20:00:54 -0800

    Add NP-hard parking lot search planning utility

--- modules/jb_bootcamp/jb_bootcamp/__init__.py
+++ modules/jb_bootcamp/jb_bootcamp/__init__.py
@@ -55,6 +55,7 @@ from .perfume_maker import *
 from .colorless_map import *
 from .happiness_formula_drinks import *
 from .toxicity import *
+from .parking_lot_search import *
 
 __author__ = 'Justin Bois'
 __email__ = 'bois@caltech.edu'
